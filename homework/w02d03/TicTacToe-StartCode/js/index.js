d14b6b9e2b781d2eb4717747c94a05674a8f0ed5
Kane Marcus <kaneofthrones@gmail.com>
2017-08-09 17:09:50 +0100

    push test

---
+++ homework/w02d03/TicTacToe-StartCode/js/index.js
@@ -0,0 +1,45 @@
+//pseudo code
+
+var player1 = [];
+var player2 = [];
+var winConditions = [[012], [345], [678], [036], [147], [258], [048], [642]];
+
+//store win coditions in an array of arrays
+//012, 345, 678, 036, 147, 258, 048, 642
+//
+//declare global variables
+var boxes = $('td');
+//make functions
+
+boxes.each.addClass('.X');
+
+//display X's
+function displayPlayer1(){
+// 	$(boxes).on('click', function(){
+// 		console.log('button pressed');
+
+// 	var boxes = $("td");
+//     $(boxes).each(function(index,box){      
+    
+//   })
+// })
+}
+
+
+//display O's 
+function displayPlayer2(){
+
+}
+//make function to change html to say, whos turn it is
+
+//win condition function
+
+//setup event listeners
+
+//alernate the input for each click
+
+//run function
+
+function run() {
+	displayPlayer1();
+}

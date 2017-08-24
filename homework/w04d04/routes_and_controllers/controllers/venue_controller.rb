b248ca34850143a2a77f432e23e966e391e04a3f
Kane Marcus <kaneofthrones@gmail.com>
2017-08-24 17:37:05 +0100

    added database

--- homework/w04d04/routes_and_controllers/controllers/venue_controller.rb
+++ homework/w04d04/routes_and_controllers/controllers/venue_controller.rb
@@ -1,3 +1,20 @@
 class PostsController < Sinatra::Base
 
+	$venues = [{
+		id: 0,
+		name: "venue1",
+		content: "nice decor",
+		},
+		{
+		id: 1,
+		name: "venue2",
+		content: "mediocre decor",
+		},
+		{
+		id: 2,
+		name: "venue3",
+		content: "banging"
+		}]
+
+
 }

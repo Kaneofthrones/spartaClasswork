a6bfd9bde71056b853f3a08dcb505970ee8a3f79
Kane Marcus <kaneofthrones@gmail.com>
2017-08-24 17:43:04 +0100

    added 7 restful routes

--- homework/w04d04/routes_and_controllers/controllers/venue_controller.rb
+++ homework/w04d04/routes_and_controllers/controllers/venue_controller.rb
@@ -16,5 +16,66 @@ class PostsController < Sinatra::Base
 		content: "banging"
 		}]
 
+	#sets root as the parent-directory of the current file
+	set :root, File.join(File.dirname(__FILE__), '..')
+
+	#sets the view directory correctly 
+	set :views, Proc.new {File.join(root, 'views') }
+
+	configure :development do 
+		register Sinatra::Reloader
+	end
+
+	get '/' do 
+
+		"index"
+
+	end
+
+
+	get '/new' do 
+
+		"new"
+
+	end
+
+	get '/:id' do 
+
+		"show"
+
+	end
+
+	post '/' do 
+
+		"create"
+
+	end 
+
+	put '/:id' do 
+
+		"update"
+
+	end
+
+	delete '/:id' do 
+
+		"delete"
+
+	end
+
+	get '/:id/edit' do 
+
+		"edit"
+
+	end 
+
+
+
+
+
+
+
+
+
 
 }

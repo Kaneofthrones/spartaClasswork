bf529b6328827358a3f1e2c746a3e7c77b95fe61
Kane Marcus <kaneofthrones@gmail.com>
2017-08-26 16:28:02 +0100

    added seed

--- homework/w04d05_mini_project/seed.sql
+++ homework/w04d05_mini_project/seed.sql
@@ -1,11 +1,15 @@
-class ReviewController < Sinatra::Base
+DROP TABLE IF EXISTS review;
 
-  # sets root as the parent-directory of the current file
-  set :root, File.join(File.dirname(__FILE__), '..')
-  
-  # sets the view directory correctly
-  set :views, Proc.new { File.join(root, "views") } 
+CREATE TABLE review (
 
-  configure :development do
-      register Sinatra::Reloader
-  end
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  body TEXT
+
+);
+
+INSERT INTO post (title , body) VALUES ('Post 1' , 'Body text 1');
+INSERT INTO post (title , body) VALUES ('Post 2' , 'Body text 2');
+INSERT INTO post (title , body) VALUES ('Post 3' , 'Body text 3');
+INSERT INTO post (title , body) VALUES ('Post 4' , 'Body text 4');
+
